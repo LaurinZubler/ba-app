e26e91f88ce3487eb270a8a643ce8712cace9039
Laurin <la.urin@hotmail.ch>
2024-05-08 17:43:28 +0200

    main: qr scanner

--- lib/view/components/camera.dart
+++ lib/view/components/camera.dart
@@ -1,11 +1,46 @@
 import 'package:flutter/material.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
 
-class Camera extends HookConsumerWidget {
-  const Camera({super.key});
+final mobileScannerControllerProvider = Provider.autoDispose<MobileScannerController>((ref) {
+    final controller = MobileScannerController();
+    ref.onDispose(() async => await controller.dispose());
+    return controller;
+  }
+);
+
+typedef OnQrError = void Function(BuildContext context, Object error);
+typedef OnQrDetect = void Function(String qr);
+
+class Camera extends ConsumerWidget {
+  const Camera({super.key, required this.onQrError, required this.onQrDetect});
+
+  final OnQrError onQrError;
+  final OnQrDetect onQrDetect;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Center(child:Text("camera"));
+    final controller = ref.watch(mobileScannerControllerProvider);
+
+    final handleBarcode = (BarcodeCapture capture) async {
+      final List<Barcode> barcodes = capture.barcodes;
+      var qr;
+      for (final barcode in barcodes) {
+        await controller.stop();
+        qr = barcode.rawValue.toString();
+      }
+      onQrDetect(qr);
+    };
+
+    return MobileScanner(
+      controller: controller,
+      errorBuilder: (context, error, child) {
+        onQrError(context, error);
+      // todo: why child?? remove?
+      // todo: i18n: permission denied
+        return child ?? Center(child: Text('Unable to access camera'));
+      },
+      onDetect: handleBarcode
+    );
   }
 }
--- lib/view/components/contact_exchange.dart
+++ lib/view/components/contact_exchange.dart
@@ -3,6 +3,7 @@ import 'package:ba_app/view/components/qr.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 enum ExchangeStateEnum { qr, camera }
 
@@ -15,9 +16,33 @@ class ContactExchange extends HookConsumerWidget {
 
     final cameraIcon = const Icon(Icons.photo_camera, size: 30);
     final qrIcon = const Icon(Icons.qr_code_2, size: 32);
+
     final isQrActive = exchangeState.value == ExchangeStateEnum.qr;
 
-    final activeWidget = isQrActive ? const Qr() : const Camera();
+    final toggleWidget = () => exchangeState.value = (isQrActive ? ExchangeStateEnum.camera : ExchangeStateEnum.qr);
+
+    final onQrError = (BuildContext context, Object error) {
+      // todo: needed?
+      debugPrint("qr error");
+    };
+
+    final onQrDetect = (String qr) {
+      debugPrint("qr scanned: $qr");
+      toggleWidget();
+
+      // todo: i18n: contact saved
+      Fluttertoast.showToast(
+        // msg: 'contact saved',
+        msg: qr,
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.CENTER,
+        timeInSecForIosWeb: 1,
+      );
+    };
+
+
+    final activeWidget = isQrActive ? const Qr() : Camera(onQrError: onQrError, onQrDetect: onQrDetect);
+
     final activeIcon = isQrActive ? cameraIcon : qrIcon;
 
     return LayoutBuilder(
@@ -35,7 +60,7 @@ class ContactExchange extends HookConsumerWidget {
             Align(
               alignment: const Alignment(0, 0.75), // middle of width, 75% of height
               child: FloatingActionButton(
-                onPressed: () => exchangeState.value = (isQrActive ? ExchangeStateEnum.camera : ExchangeStateEnum.qr),
+                onPressed: toggleWidget,
                 child: activeIcon,
               ),
             ),
