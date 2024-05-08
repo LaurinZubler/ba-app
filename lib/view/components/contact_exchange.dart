411ffcc8882fbd5bebd5d0cd0df6b80704c440d1
Laurin <la.urin@hotmail.ch>
2024-05-08 16:16:14 +0200

    main: refactor contact exchange

--- lib/view/components/contact_exchange.dart
+++ lib/view/components/contact_exchange.dart
@@ -1,29 +1,28 @@
 import 'package:ba_app/view/components/camera.dart';
 import 'package:ba_app/view/components/qr.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 enum ExchangeStateEnum { qr, camera }
-
-final exchangeStateProvider = StateProvider<ExchangeStateEnum>((ref) => ExchangeStateEnum.qr);
 
 class ContactExchange extends HookConsumerWidget {
   const ContactExchange({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    var cameraIcon = const Icon(Icons.photo_camera, size: 30);
-    var qrIcon = const Icon(Icons.qr_code_2, size: 32);
+    final exchangeState = useState(ExchangeStateEnum.qr);
 
-    var qr = const Qr();
-    var camera = const Camera();
+    final cameraIcon = const Icon(Icons.photo_camera, size: 30);
+    final qrIcon = const Icon(Icons.qr_code_2, size: 32);
+    final isQrActive = exchangeState.value == ExchangeStateEnum.qr;
 
-    isQrActive() => ref.watch(exchangeStateProvider) == ExchangeStateEnum.qr;
-    toggleExchangeState() => ref.read(exchangeStateProvider.notifier).state = (isQrActive() ? ExchangeStateEnum.camera : ExchangeStateEnum.qr);
+    final activeWidget = isQrActive ? const Qr() : const Camera();
+    final activeIcon = isQrActive ? cameraIcon : qrIcon;
 
     return LayoutBuilder(
       builder: (context, constraints) {
-        double cardSize = constraints.maxWidth;
+        final cardSize = constraints.maxWidth;
 
         return Stack(
           alignment: Alignment.center,
@@ -31,13 +30,13 @@ class ContactExchange extends HookConsumerWidget {
             SizedBox(
               width: cardSize,
               height: cardSize,
-              child: Card(child: isQrActive() ? qr : camera),
+              child: Card(child: activeWidget),
             ),
             Align(
-              alignment: Alignment(0, 0.75), // middle of width, 75% of height
+              alignment: const Alignment(0, 0.75), // middle of width, 75% of height
               child: FloatingActionButton(
-                onPressed: toggleExchangeState,
-                child: isQrActive() ? cameraIcon : qrIcon,
+                onPressed: () => exchangeState.value = (isQrActive ? ExchangeStateEnum.camera : ExchangeStateEnum.qr),
+                child: activeIcon,
               ),
             ),
           ],
