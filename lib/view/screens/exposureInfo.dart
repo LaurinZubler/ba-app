09520f67bbb8a5d55a20a208876687d88ec479a3
Laurin <la.urin@hotmail.ch>
2024-05-22 19:53:01 +0200

    exposure info scroll

--- lib/view/screens/exposureInfo.dart
+++ lib/view/screens/exposureInfo.dart
@@ -10,7 +10,6 @@ class ExposureInfoView extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-
     const sti = STI(key: "smilingSyndrome", numberOfSymptoms: 3);
 
     final symptomLabels = [
@@ -26,131 +25,136 @@ class ExposureInfoView extends HookConsumerWidget {
 
     return Theme(
       data: UpsiTheme.red,
-      child: Builder(
-        builder: (context) {
-          return Scaffold(
-            // extendBodyBehindAppBar: true,
-            appBar: AppBar(
-              systemOverlayStyle: UpsiTheme.systemUiOverlayStyle,
-            ),
-            body: Container(
-              padding: const EdgeInsets.symmetric(horizontal: 32),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
+      child: Builder(builder: (context) {
+        return Scaffold(
+          appBar: AppBar(
+            systemOverlayStyle: UpsiTheme.systemUiOverlayStyle,
+          ),
+          body: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 32),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 16),
+                  child: Text(
                     AppLocalizations.of(context)!.sti_name(sti.key),
                     style: Theme.of(context).textTheme.displayLarge,
                   ),
-
-                  Padding(
-                    padding: const EdgeInsets.only(top: 48),
-                    child: Text(
-                      AppLocalizations.of(context)!.exposureInfo_actions,
-                      style: Theme.of(context).textTheme.titleLarge,
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 16, left: 8),
+                ),
+                Expanded(
+                  child: SingleChildScrollView(
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text('• ${AppLocalizations.of(context)!.exposureInfo_action1}'),
-                        Text('• ${AppLocalizations.of(context)!.exposureInfo_action2}'),
-                        Text('• ${AppLocalizations.of(context)!.exposureInfo_action3}'),
-                        Text('• ${AppLocalizations.of(context)!.exposureInfo_action4}'),
-                      ],
-                    )
-                  ),
-
-                  Padding(
-                    padding: const EdgeInsets.only(top: 24),
-                    child: Text(
-                      AppLocalizations.of(context)!.exposureInfo_needSupport,
-                      style: Theme.of(context).textTheme.titleLarge,
-                    ),
-                  ),
-                  Padding(
-                      padding: const EdgeInsets.only(top: 16),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(AppLocalizations.of(context)!.exposureInfo_hotline_label),
-                              Text(
-                                AppLocalizations.of(context)!.exposureInfo_hotline_nr,
-                                style: Theme.of(context).textTheme.titleSmall,
-                              )
-                            ],
+                        Padding(
+                          padding: const EdgeInsets.only(top: 32),
+                          child: Text(
+                            AppLocalizations.of(context)!.exposureInfo_actions,
+                            style: Theme.of(context).textTheme.titleLarge,
                           ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 16, left: 8),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
-                              Text(AppLocalizations.of(context)!.exposureInfo_web_label),
-                              Text(
-                                AppLocalizations.of(context)!.exposureInfo_web_url,
-                                style: Theme.of(context).textTheme.titleSmall,
+                              Text('• ${AppLocalizations.of(context)!.exposureInfo_action1}'),
+                              Text('• ${AppLocalizations.of(context)!.exposureInfo_action2}'),
+                              Text('• ${AppLocalizations.of(context)!.exposureInfo_action3}'),
+                              Text('• ${AppLocalizations.of(context)!.exposureInfo_action4}'),
+                            ],
+                          )
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 24),
+                          child: Text(
+                            AppLocalizations.of(context)!.exposureInfo_needSupport,
+                            style: Theme.of(context).textTheme.titleLarge,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 16),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(AppLocalizations.of(context)!.exposureInfo_hotline_label),
+                                  Text(
+                                    AppLocalizations.of(context)!.exposureInfo_hotline_nr,
+                                    style: Theme.of(context).textTheme.titleSmall,
+                                  )
+                                ],
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(AppLocalizations.of(context)!.exposureInfo_web_label),
+                                  Text(
+                                    AppLocalizations.of(context)!.exposureInfo_web_url,
+                                    style: Theme.of(context).textTheme.titleSmall,
+                                  )
+                                ],
                               )
                             ],
                           )
-                        ],
-                      )
-                  ),
-
-                  Padding(
-                    padding: const EdgeInsets.only(top: 24),
-                    child: Text(
-                      AppLocalizations.of(context)!.exposureInfo_symptoms,
-                      style: Theme.of(context).textTheme.titleLarge,
-                    ),
-                  ),
-                  Expanded(
-                    child: ListView.builder(
-                      itemCount: symptomLabels.length,
-                      itemBuilder: (context, index) {
-                        return Padding(
-                          padding: const EdgeInsets.only(top: 16),
-                          child: Row(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Expanded(
-                                flex: 2,
-                                child: Text(
-                                  symptomLabels[index],
-                                  style: const TextStyle(fontWeight: FontWeight.bold),
-                                ),
-                              ),
-                              const SizedBox(width: 8),
-                              Expanded(
-                                flex: 3,
-                                child: Text(symptomDescriptions[index]),
-                              ),
-                            ],
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 24),
+                          child: Text(
+                            AppLocalizations.of(context)!.exposureInfo_symptoms,
+                            style: Theme.of(context).textTheme.titleLarge,
                           ),
-                        );
-                      },
+                        ),
+                        ListView.builder(
+                          shrinkWrap: true,
+                          physics: const NeverScrollableScrollPhysics(),
+                          itemCount: symptomLabels.length,
+                          itemBuilder: (context, index) {
+                            return Padding(
+                              padding: const EdgeInsets.only(top: 16),
+                              child: Row(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Expanded(
+                                    flex: 2,
+                                    child: Text(
+                                      symptomLabels[index],
+                                      style: const TextStyle(fontWeight: FontWeight.bold),
+                                    ),
+                                  ),
+                                  const SizedBox(width: 8),
+                                  Expanded(
+                                    flex: 3,
+                                    child: Text(symptomDescriptions[index]),
+                                  ),
+                                ],
+                              ),
+                            );
+                          },
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 24),
+                          child: Text(
+                            AppLocalizations.of(context)!.exposureInfo_moreInformation,
+                            style: Theme.of(context).textTheme.titleLarge,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 16, bottom: 32),
+                          child: Text(AppLocalizations.of(context)!.sti_moreInformation(sti.key)),
+                        )
+                      ],
                     ),
                   ),
-
-                  Padding(
-                    padding: const EdgeInsets.only(top: 24),
-                    child: Text(
-                      AppLocalizations.of(context)!.exposureInfo_moreInformation,
-                      style: Theme.of(context).textTheme.titleLarge,
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 16),
-                    child: Text(AppLocalizations.of(context)!.sti_moreInformation(sti.key)),
-                  )
-                ],
-              ),
+                ),
+              ],
             ),
-          );
-        }
-      )
+          ),
+        );
+      })
     );
   }
 }
