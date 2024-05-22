4ef530cabfb64b9f995546dd6721af2203069b5a
Laurin <la.urin@hotmail.ch>
2024-05-22 20:08:16 +0200

    exposure as DraggableScrollableSheet

--- lib/view/screens/home.dart
+++ lib/view/screens/home.dart
@@ -50,9 +50,8 @@ class HomeView extends HookConsumerWidget {
 
     final activeIcon = isQrActive ? cameraIcon : qrIcon;
 
-    final AnimationController controller = useAnimationController(
-      duration: const Duration(seconds: 3),
-    );
+    final DraggableScrollableController sheetController = DraggableScrollableController();
+    final List<String> entries = <String>['A', 'B', 'C'];
 
     return Scaffold(
       extendBodyBehindAppBar: true,
@@ -66,7 +65,8 @@ class HomeView extends HookConsumerWidget {
         ],
       ),
       body: Container(
-        padding: const EdgeInsets.all(32),
+        // padding: const EdgeInsets.all(32),
+        padding: const EdgeInsets.only(left: 24, top: 24, right: 24),
         decoration: UpsiTheme.homeBackground,
         child: LayoutBuilder(builder: (context, constraints) {
           final cardSize = constraints.maxWidth;
@@ -94,51 +94,121 @@ class HomeView extends HookConsumerWidget {
                   child: activeIcon,
                 ),
               ),
+              DraggableScrollableSheet(
+                controller: sheetController,
+                initialChildSize: 0.12,
+                minChildSize: 0.12,
+                maxChildSize: 0.8,
+                snap: true,
+
+                builder: (BuildContext context, ScrollController scrollController) {
+
+                  final secondaryColor = Theme.of(context).colorScheme.secondary;
+                  final border = BorderSide(
+                    color: secondaryColor,
+                    width: 4,
+                  );
+
+                  return Container(
+                    padding: const EdgeInsets.only(left: 24, top: 24, right: 24),
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).canvasColor,
+                      borderRadius: const BorderRadius.vertical(top: Radius.circular(16)),
+                    ),
+                    child:
+
+                    // Column(
+                    //   children: [
+
+                        // GestureDetector(
+                        //   // onVerticalDragUpdate: onVerticalDragUpdate,
+                        //   child: Container(
+                        //     width: double.infinity,
+                        //     // color: colorScheme.onSurface,
+                        //     child: Align(
+                        //       alignment: Alignment.topCenter,
+                        //       child: Container(
+                        //         margin: const EdgeInsets.symmetric(vertical: 8.0),
+                        //         width: 32.0,
+                        //         height: 4.0,
+                        //         decoration: BoxDecoration(
+                        //           // color: colorScheme.surfaceContainerHighest,
+                        //           borderRadius: BorderRadius.circular(8.0),
+                        //         ),
+                        //       ),
+                        //     ),
+                        //   ),
+                        // ),
+
+                        Container(
+                          decoration: BoxDecoration(
+                            border: Border(top: border, left: border, right: border),
+                            borderRadius: const BorderRadius.vertical(top: Radius.circular(8)),
+                          ),
+                          child: Expanded(
+                            child: SingleChildScrollView(
+                              child: Column(
+                                // controller: scrollController,
+                                children: [
+                                  ListTile(
+                                    iconColor: secondaryColor,
+                                    textColor: secondaryColor,
+                                    visualDensity: VisualDensity.comfortable,
+                                    // contentPadding: const EdgeInsets.only(left: 16, right: 8),
+                                    // horizontalTitleGap: 8,
+
+                                    titleTextStyle: const TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontFamily: UpsiTheme.sfProDisplay,
+                                    ),
+
+                                    leading: const Icon(Icons.warning_rounded, size: 32),
+                                    title: FittedBox(
+                                        fit: BoxFit.fitWidth,
+                                        alignment: Alignment.centerLeft,
+                                        child: Text(AppLocalizations.of(context)!.home_warning)),
+                                  ),
+
+
+                                  // ListView.builder(
+                                  //   shrinkWrap: true,
+                                  //   physics: const NeverScrollableScrollPhysics(),
+                                  //   itemCount: entries.length,
+                                  //   itemBuilder: (BuildContext context, int index) {
+                                  //     return ListTile(
+                                  //       title: Text(entries[index])
+                                  //     );
+                                  //   }
+                                  // ),
+
+                                  // Flexible(
+                                  //     child: ListView.builder(
+                                  //         padding: const EdgeInsets.all(8),
+                                  //         itemCount: entries.length,
+                                  //         controller: scrollController,
+                                  //         itemBuilder: (BuildContext context, int index) {
+                                  //           return ListTile(
+                                  //               title: Text(entries[index])
+                                  //           );
+                                  //         }
+                                  //     )
+                                  // )
+                                ],
+                              ),
+                            )
+                          ),
+
+
+
+                      )
+                      // ]
+                    // )
+                  );
+                },
+              ),
             ]
           );
         })
-      ),
-      bottomSheet: BottomSheet(
-        onClosing: () {  },
-        animationController: controller,
-        builder: (BuildContext context) {
-          final secondaryColor = Theme.of(context).colorScheme.secondary;
-          final border = BorderSide(
-            color: secondaryColor,
-            width: 4,
-          );
-
-          return GestureDetector(
-              onTap: () => Navigator.of(context).push(MaterialPageRoute(builder: (context) => const ExposureInfoView())),
-              child: Container(
-                margin: const EdgeInsets.only(left: 24, top: 24, right: 24),
-
-                decoration: BoxDecoration(
-                  border: Border(top: border, left: border, right: border),
-                  borderRadius: const BorderRadius.vertical(top: Radius.circular(8)),
-                ),
-                child: ListTile(
-                  iconColor: secondaryColor,
-                  textColor: secondaryColor,
-                  visualDensity: VisualDensity.comfortable,
-
-
-                  titleTextStyle: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontFamily: UpsiTheme.sfProDisplay,
-                  ),
-
-                  leading: const Icon(Icons.warning_rounded, size: 32),
-                  trailing: const Icon(Icons.arrow_right),
-
-                  title: Text(
-                    AppLocalizations.of(context)!.home_warning,
-                    style: Theme.of(context).textTheme.titleLarge
-                  ),
-                ),
-            )
-          );
-        },
       ),
     );
   }
