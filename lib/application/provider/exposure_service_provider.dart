0d17f3197a3d438c4ad140d90bd63a67220d6259
Laurin <la.urin@hotmail.ch>
2024-06-04 13:48:09 +0200

    provider

--- lib/application/provider/exposure_service_provider.dart
+++ lib/application/provider/exposure_service_provider.dart
@@ -1,6 +1,7 @@
 import 'package:ba_app/application/provider/contact_service_provider.dart';
 import 'package:ba_app/application/provider/cryptography_service_provider.dart';
 import 'package:ba_app/application/provider/push_notification_service_provider.dart';
+import 'package:ba_app/application/provider/upsi_contract_service_provider.dart';
 import 'package:ba_app/application/service/exposure_service.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'exposure_repository_provider.dart';
@@ -10,5 +11,6 @@ final exposureServiceProvider = Provider<ExposureService>((ref) {
   final contactService = ref.watch(contactServiceProvider);
   final cryptographyService = ref.watch(cryptographyServiceProvider);
   final pushNotificationService = ref.watch(pushNotificationServiceProvider);
-  return ExposureService(exposureRepository, contactService, cryptographyService, pushNotificationService);
+  final upsiContractProvider = ref.watch(upsiContractServiceProvider);
+  return ExposureService(exposureRepository, contactService, cryptographyService, pushNotificationService, upsiContractProvider);
 });
---
+++ lib/application/provider/upsi_contract_service_provider.dart
@@ -0,0 +1,7 @@
+import 'package:ba_app/application/global.dart';
+import 'package:ba_app/application/service/upsi_contract_service.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+final upsiContractServiceProvider = Provider<UpsiContractService>((ref) {
+  return UpsiContractService(Global.upsiContractABI);
+});
